d553fb1899ef19c281ec5dd01abfee476968e95c
Alan Andres Garcia Castillo <alngc980@gmail.com>
2023-10-06 18:29:29 -0500

    Actualizar y eliminar recurso de autores

--- Controllers/AutoresController.cs
+++ Controllers/AutoresController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 using WebAPIAutores.Entidades;
 
@@ -30,5 +31,38 @@ namespace WebAPIAutores.Controllers
             return Ok();
         }
 
+        [HttpPut("{id:int}")] //api/autore/1
+        public async Task<ActionResult> Put(Autor autor, int id)
+        {
+            if(autor.Id != id)
+            {
+                return BadRequest("El id del autor no coincide con el id de la URL");
+            }
+            
+            var existe = await context.Autores.AnyAsync(x => x.Id == id);
+            if (!existe)
+            {
+                return NotFound();
+            }
+
+            context.Update(autor);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var existe =  await context.Autores.AnyAsync(x => x.Id == id);
+            if(!existe)
+            {
+                return NotFound();
+            }
+
+            context.Remove(new Autor() { Id = id });
+            await context.SaveChangesAsync();
+            return Ok();
+            
+        }
     }
 }
